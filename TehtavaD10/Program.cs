57fcdb7030683d78dd7c88f454ee4ae42eb13e63
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-08-24 15:54:45 +0300

    Add project files.

---
+++ TehtavaD10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace KertotauluOhjelma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Syötä kokonaisluku välillä 1 - 10: ");
+                int luku = int.Parse(Console.ReadLine());
+
+                // Tarkistetaan, että syöte on sallitulla välillä
+                if (luku >= 1 && luku <= 10)
+                {
+                    // Tulostetaan kertotaulu käyttäen for-silmukkaa
+                    for (int i = 1; i <= 10; i++)
+                    {
+                        int tulos = luku * i;
+                        Console.WriteLine($"{luku} x {i} = {tulos}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Syöte ei ole sallitulla välillä.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Virheellinen syöte. Syötä kokonaisluku.");
+            }
+        }
+    }
+}
+
